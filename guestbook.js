5d28dbc1889654906c8772f5601848cb1cc889d8
TimWongHP <66864819+TimWongHP@users.noreply.github.com>
2022-07-22 19:04:16 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://705dd27d.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
